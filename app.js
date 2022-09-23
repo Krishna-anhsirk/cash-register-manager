1efe100584caf6c7286d1f93810326d5c4dbf292
Krishna-anhsirk <Krishnatejajinka@gmail.com>
2022-09-23 11:35:20 +0530

    HTML and JS done

---
+++ app.js
@@ -0,0 +1,39 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const check = document.querySelector(".check-money");
+const message = document.querySelector(".message");
+const notes = document.querySelectorAll(".notes");
+const displayChange = document.querySelector(".display-change");
+
+const givenNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+function validateCash() {
+  //To hide the message
+  showMessage("");
+  //Checking if bill amount is valid
+  if (billAmount.value > 0) {
+    //Checking if given cash is greater than bill
+    const remainingChange = cashGiven.value - billAmount.value;
+    if (remainingChange >= 0) {
+      displayChange.innerText = remainingChange;
+      returnChange(remainingChange);
+    } else {
+      showMessage("Do you wanna wash the plates ?");
+    }
+  } else {
+    showMessage("Enter valid bill amount!");
+  }
+}
+
+function returnChange(change) {
+  for (let i = 0; i < givenNotes.length; i++) {
+    notes[i].innerText = Math.trunc(change / givenNotes[i]);
+    change = change % givenNotes[i];
+  }
+}
+
+check.addEventListener("click", validateCash);
+
+function showMessage(errorMessage) {
+  message.innerText = errorMessage;
+}
